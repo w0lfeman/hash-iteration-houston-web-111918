261fc85fd163be8d0ef7c37d1b6e6b29b21e14b4
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-20 03:31:57 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,24 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
+winner  
+end
+
+
+birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27 }
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end 
+end   
+
+
+
